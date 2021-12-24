e55b1b523dbe7954bcd7c343dd4d758d0b59ae28
Eda Toktogulova <edatoktogulova2003@gmail.com>
2021-12-24 12:54:27 +0600

    added var

--- script.js
+++ script.js
@@ -1,6 +1,61 @@
-var example1 = 10 === 10; // 
-var example2
-var example3
-var example4
-var example5
-var example6
+// var example1 = 10 === 10; // equals
+// var example2 = 11 > 10; // more than
+// var example3 = 10 < 11; // less than
+// var example4 = 10 >= 10; // more than or equal
+// var example5 = 10 <= 10; // less than or equal
+// var example6 = 11 !== 10; // not equals
+
+// var a = 15;
+// var b = 10;
+// var example7 = a < b;
+
+// var c = 10;
+// var d = 11;
+// if (c < d) { // true
+//     alert("C is less than D");
+// }
+
+// var e = 3.14;
+// if (e !== 3.14) {
+//     alert("Why don't you know number PI?"); // true
+// }
+// else {
+//     alert("Good job!")//false
+// }
+
+// var age = 10;
+// if (age >= 18) {
+//     alert("You are too young to vote!");
+// }
+
+// var lightSwitch = 1;
+// if (lightSwitch === 1) {
+//     alert 
+// } 
+
+// var username = prompt("What i s your name?", "stranger");
+// alert("Welcome" + username);
+
+// var apple = 6;
+// var pear = 5;
+// if (apple > pear) {
+//     alert ("")
+// }
+// else {
+//     alert("")
+// }
+
+// var a = 10;
+// var b = 12;
+
+
+// var usesrname = prompt ("What is your name");
+// alert("Welcome" + usesrname);
+
+
+
+var c = 13;
+var d = 12;
+if (c < d) {
+    alert("C is less than D");
+}
